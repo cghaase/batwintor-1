218827a9c46f2253a0b671c0612231871331716e
cReedHranac <crh244@gmail.com>
2017-05-17 10:50:53 +1200

    First commit to Katie's branch of dirty magic including updated arousal energy function, and new arousal time, cool time, and cool enegry functions.

--- R/CalcEnergyArousal_fun.R
+++ R/CalcEnergyArousal_fun.R
@@ -1,17 +1,23 @@
 #' Calculate energy needed during an arousal from torpor
 #'
-#' \code{CalcEnergyArousal} Calculate the enegry required to maintain
-#'  eurthermic temperatures during an arousal from torpor.
-#'
-#' @param Ttor temperature of torpor
+#' \code{CalcEnegryArousal} Calculate the energy required to arouse from torpor
+#' and enter an euthermic state.
+#' @param Ta ambient temperature
 #' @param bat.params list of bat parameters output from \code{\link{BatLoad}}
 #'
-#' @return returns enegry required to arouse
+#' @return returns enegry required to arouse from torpor
 #'
 #' @example ExampleScripts/CalcEnergyArousal_ex.R
 #'
+
+#' @details Extraced from McKechnie & Wolf 2004 equation 3
+#' @example ExampleScripts/CalcEnegryArousal_ex.R
+#' @author Katie Haase
 CalcEnergyArousal <- function(Ttor, bat.params){
   with(bat.params,{
-    (Teu - Ttor) * S
+    ifelse(Ta > Ttormin,
+           (mass*(Teu - Ta)*S) + ((Teu - Ta)/WR)*(TMRmin + ((RMR - TMRmin)/2)))
+           (mass*(Teu - Ttomin)*S) + ((Teu - Ttormin)/WR)*(TMRmin + ((RMR -
+            TMRmin)/2))
   })
 }
