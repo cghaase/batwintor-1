1fcef5defa4e0fea47ab6d4e65b808256cf4f85e
cReedHranac <crh244@gmail.com>
2017-05-16 14:16:24 +1200

    Model functions, coded in. Still require more documentation and test scripts. NB: Do I need EnegryPd or has that just been replaced by DynamicEnegry and DetModel?

---
+++ R/ConvertToFat_fun.R
@@ -0,0 +1,15 @@
+#' Convert kCal to grams of fat
+#'
+#' \code{ConvertToFat} converts kCal to grams of fat.
+#'
+#' @param enegry amount of enegry to convert in kCal
+#' @param k1 constant 1
+#' @param k2 constant 2
+#' @param k3 constant 3
+#'
+#' @details TODO check and make sure units are correct
+#'
+#' @example ConvertToFat(enegry = 9)
+ConvertToFat <- function(enegry, k1 = 20.1, k2 = 39.3, k3 = 100){
+  enegry*k1/(k2*k3)
+}
