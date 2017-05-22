ca81888d1827aefb29f24b9575887b07d50515a4
Carter Reed Hranac <crh244@gmail.com>
2017-05-22 15:11:06 +1200

    Begin documentation of data that will ship with package and slight tweeks to load functions to handle new systems

---
+++ R/BatDat.R
@@ -0,0 +1,51 @@
+#' Metabolic paramerters for 6 species of hibernating bats.
+#'
+#' A dataset containing information on euthermic and torpid metabolic rates of
+#' 6 species of hibernating bats.
+#'
+#' @format a dataset containg 14 measurements for 6 species
+#' \describe{
+#'   \item{RMR}{esting metabolic rate (TODO Units)}
+#'   \item{TMRmin}{minimum metabolic rate muring torpor (TODO units)}
+#'   \item{Teu}{euthermic temperature in degress C}
+#'   \item{Tlc}{lower critical temperature in degrees C}
+#'   \item{Ttormin}{Temperature at which TMRmin is achieved in degrees C}
+#'   \item{Ceu}{conductance during euthermic temperatures (TODO units)}
+#'   \item{Ct}{conductance during torpor (TODO units)}
+#'   \item{S}{specific heat of tissue (TODO units)}
+#'   \item{ttormax}{maximal length of time for a bout of torpor in hours}
+#'   \item{tar}{time take to arouse from torpor in hours}
+#'   \item{teu}{time spent euthermic during a bout of torpor in hours}
+#'   \item{mass}{animal mass in grams}
+#'   \item{WR}{warming rate from torpor to euthermic temperature in degrees C/
+#'     hour}
+#'   \item{CR}{cooling rate from euthermic temperature to torpor in degrees C/
+#'     hour}
+#'   }
+#' @details Naming of variables is largely drawn from the "Physiological
+#' Ecology and Energetics of Bats" by Speakman and Thomas contained within
+#' Bat Ecology.
+
+"bat.params"
+bats <- c("M.lucifigus", "M.myotis", "E.serotinus", "E.fuscus", "M.yumanensis",
+  "M.californicus")
+RMR <- c(2.6,	1, 1.596296296, 1.13, 2.301, 3.425)
+TMRmin <- c(0.03,	0.2, 0.026431718,	0.028, 0.0467, 0.033)
+Teu <- c(35, 35, 35, 35, 33.4, 32.4)
+Tlc <- rep(32, 6)
+Ttormin <- c(2, 10, 6, 3.5, 4, 6)
+Ceu <- rep(.2638, 6)
+Ct <- rep(.055, 6)
+S <- rep(.131, 6)
+ttormax <- c(rep(792,5),408)
+tar <- rep(.75,6)
+teu <- c(rep(3,5),2.1666)
+mass <- c(9, 25, 22.7, 15, 5.55, 5.4)
+WR <- rep(NA, 6)
+CR <- rep(NA, 6)
+bat.params <- cbind(RMR = RMR, TMRmin = TMRmin, Teu = Teu, Tlc = Tlc,
+  Ttormin = Ttormin, Ceu = Ceu, Ct = Ct, S = S, ttormax = ttormax, tar = tar,
+  teu = teu, mass = mass, WR = WR, CR = CR)
+rownames(bat.params) <- bats
+
+devtools::use_data(bat.params, overwrite = T)
---
+++ R/FungDat.R
@@ -0,0 +1,24 @@
+#' Growth coeffecients for funal growth models
+#'
+#' The beta and mu growth coeffecients for two different models of fungal growth.
+#'
+#' @format a data set including 3 beta parameters and 2 mu parameters for 2
+#'  models
+#' \describe{
+#'   \item{beta1}{beta1}
+#'   \item{beta2}{beta2}
+#'   \item{beta3}{beta3}
+#'   \item{mu1}{mu1}
+#'   \item{mu2}{mu2}
+#' }
+"fung.params"
+beta1 <- c(0.0007751467,	0.0001762951)
+beta2 <- c(0.2699683006,	0.2699682306)
+beta3 <- c(19.7303882961,	19.7303885569)
+mu1 <- c(0.000150958,	0.000150958)
+mu2 <- c(-0.0099245944,	-0.0099245944)
+fung.params <- cbind(beta1 = beta1, beta2 = beta2, beta3 = beta3, mu1 = mu1,
+  mu2 = mu2)
+rownames(fung.params) <- c("Chaturvedi", "Verant")
+
+devtools::use_data(fung.params, overwrite = T)
