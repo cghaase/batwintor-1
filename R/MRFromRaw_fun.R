9056f30d36897a61cb1e54ff6e64d80d2cfcec9f
cReedHranac <crh244@gmail.com>
2017-05-29 11:53:52 +1200

    update for MRFromRaw, and addition of MRUpdate with attempt of example.

--- R/MRFromRaw_fun.R
+++ R/MRFromRaw_fun.R
@@ -5,9 +5,11 @@
 #'
 #' @param x a dataframe
 #' @param species species name or code (must be contained within \code{x})
-#' @param plot option to provide a plot of the parameter estimations
-#'
-MRFromRaw <- function(x, sp, plot = T){
+#' @return Object of class \code{MRFromRaw} which contains adjusted estimates
+#' of a species resting metabolic rate (RMR), torpor metabolic rate minimum
+#' (TMRmin), the temperature at which that rate is achieved (Ttormin), and
+#' average mass of sampled individuals.
+MRFromRaw <- function(x, sp){
   require(dplyr);require(mixtools)
   '%!in%' <- function(x,y)!('%in%'(x,y))
   if(sp %!in% levels(x$species)){
@@ -18,23 +20,30 @@ MRFromRaw <- function(x, sp, plot = T){
   temp <- levels(sp.df$Ta)
   sp.x.temp <- data.frame(matrix(nrow = length(temp), ncol = 4))
   names(sp.x.temp) <- c("Ta", "MR", "mass", "n")
+  # sp.l <- list()
   for(i in 1:length(temp)){
     sp.df.t <- sp.df %>%
       filter(Ta == temp[i])
     sp.spEM <- spEMsymloc(sp.df.t$VO2.ml.h.g, mu0=quantile(sp.df.t$VO2.ml.h.g,
                                                            c(.25,.75)),stochastic=TRUE)
-    if(plot == T){
-      plot(sp.spEM, title =paste0(sp," spEm Density Curves ",temp[i]," degress"))
-    }
     sp.x.temp[i,] <- c(temp[i], sp.spEM$mu[1], mean(sp.df.t$mass.in),
                        nrow(sp.df.t))
+    # assign(paste0(sp,".",i),sp.spEM)
+    # sp.l <- c(sp.l, paste0(sp,".",i))
   }
-  df <- as.data.frame(sapply(sp.x.temp, as.numeric))
-  dat.out <- data.frame(RMR = df[which(df$Ta == 35),]$MR,
-                        TMRmin = min(df$MR),
-                        Ttormin = df[which(df$MR == min(df$MR)),]$Ta,
-                        mass = weighted.mean(df$mass, sapply(df$n, FUN =
-                                                               function(x){x/max(df$n)}))
-  )
-  return(dat.out)
+  ddf <- as.data.frame(sapply(sp.x.temp, as.numeric))
+  dat.out <- data.frame(RMR = ddf[which(ddf$Ta == 35),]$MR,
+                        TMRmin = min(ddf$MR),
+                        Ttormin = ddf[which(ddf$MR == min(ddf$MR)),]$Ta,
+                        mass = weighted.mean(ddf$mass, sapply(ddf$n, FUN =
+                                                    function(x){x/max(ddf$n)})))
+  # objs <- dynGet(sp.l)
+  # out <- list(MR.est = dat.out, unlist(objs))
+  out <- dat.out
+  # out <- list(RMR = dat.out$RMR,
+  #             TMRmin = dat.out$TMRmin,
+  #             Ttormin = dat.out$Ttormin,
+  #             mass  = dat.out$mass)
+   class(out) <- c("MRFromRaw", "data.frame")
+  return(out)
 }
---
+++ R/MRUpdate_fun.R
@@ -0,0 +1,27 @@
+#' Updates build-in metabolic parameter estimates using new data.
+#'
+#' \code{MRUpdate} updates paremeter estimates supplied in
+#' \code{\link{bat.params}} with estimates obtained through the use of
+#' \code{\link{MRFromRaw}} for use throughout the package.
+#'
+#' @param x object returned by \code{\link{MRFromRaw}}
+#' @param species a character vector of the species to be updated. This should
+#' be a row name from the \code{bat.params}
+#' @param params a dataframe of metabolic parameters (generally that returned
+#' with \code{\link{data("bat.params")}})
+#'
+#' @details Generally speaking not all of the metabolic parameters documented
+#' in \code{\link{data("bat.params")}}) are updated at once, and therefore
+#' only those that have new estimates are updated.
+#'
+#' @seealso \code{\link{data("bat.params")}}), \code{\link{MRFromRaw}},
+#' \code{\link{BatLoad}}
+#' @example ExampleScripts/MRUpdate_ex.R
+MRUpdate <- function(x, species, params = bat.params){
+  bat.update <- params
+  for(i in 1:length(names(x))){
+    bat.update[species,names(x)[i]] <- x[,names(x)[i]]
+  }
+  return(bat.update)
+}
+
