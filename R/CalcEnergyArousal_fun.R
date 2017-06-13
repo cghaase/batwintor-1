ad4c968876ae486e2c69578a84c9713f28edf0f3
Catherine G. Haase <cghaase@gmail.com>
2017-06-13 16:41:31 -0600

    Updated calcEnergyArousal
    Updated the arousal function to include the costs of rewarming.

--- R/CalcEnergyArousal_fun.R
+++ R/CalcEnergyArousal_fun.R
@@ -3,15 +3,20 @@
 #' \code{CalcEnergyArousal} Calculate the enegry required to maintain
 #'  eurthermic temperatures during an arousal from torpor.
 #'
-#' @param Ttor temperature of torpor
+#' @param Ta temperature 
 #' @param bat.params list of bat parameters output from \code{\link{BatLoad}}
 #'
 #' @return returns enegry required to arouse
 #'
 #' @example ExampleScripts/CalcEnergyArousal_ex.R
 #'
-CalcEnergyArousal <- function(Ttor, bat.params){
-  with(bat.params,{
-    (Teu - Ttor) * S
-  })
+
+calcEnergyArousal <- function(Ta, params){
+	with(as.list(params),{
+		ifelse(Ta > Ttormin, 
+					(mass*(Teu - Ta)*S) + ((Teu - Ta)/WR)*(TMRmin + ((RMR - TMRmin)/2)),                  #McKechnie & Wolf 2004 equation 3
+					(mass*(Teu - Ttormin)*S) + ((Teu - Ttormin)/WR)*(TMRmin + ((RMR - TMRmin)/2)))
+		}
+	)			
 }
+
