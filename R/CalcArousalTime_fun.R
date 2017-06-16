eb01246859dd2b250316a55950da079712e06700
cReedHranac <crh244@gmail.com>
2017-06-16 14:21:15 +1200

    Doc tweeks and other such minor revisions for help files.

--- R/CalcArousalTime_fun.R
+++ R/CalcArousalTime_fun.R
@@ -1,15 +1,18 @@
-#' Calculate time to arouse to euthermic temperatures
+#' Calculate time to arouse from torpor to euthermic temperatures
 #'
 #' \code{CalcArousalTime} Calculates time to arouse to euthermic conditions
+#' given ambient conditions
 #'
 #' @param Ta ambient temperature
 #' @param bat.params list of parameters output from \code{\link{BatLoad}}
 #'
 #' @details
 #' @examples ExampleScripts/CalcArousalTime_ex.R
-#'
+#' @family Arousal Functions
+#' @seealso \code{\link{CalcEnergyArousal}}, \code{\link{CalcCoolTime}},
+#' \code{\link{CalcEnergyCool}}, \code{\link{CalcEnergyTimeEuthermic}}
 #' @references McKechnie & Wolf 2004 equation 3
-
+#'
 CalcArousalTime <- function(Ta, bat.params){
   with(bat.params,{
     ifelse(Ta > Ttormin,
--- R/CalcCoolTime_fun.R
+++ R/CalcCoolTime_fun.R
@@ -1,18 +1,20 @@
-#' Calculate time to cool from euthermic temperatures
+#' Calculate time to cool from euthermic to torpor.
 #'
-#' \code{CalcCoolTime} Calculates time to cool from euthermic conditions
+#' \code{CalcCoolTime} Calculates time to cool from euthermic conditions to
+#' torpid state given the ambient temperature.
 #'
 #' @param Ta ambient temperature
 #' @param bat.params list of parameters output from \code{\link{BatLoad}}
-#'
+#' @return Time in hours.
 #' @details
 #' @examples ExampleScripts/CalcCoolTime_ex.R
 #'
 #' @references McKechnie & Wolf 2004 equation 3
-#'
+#' @family Arousal Functions
+#' @seealso \code{\link{CalcArousalTime}}, \code{\link{CalcEnergyCool}},
+#' \code{\link{CalcEnergyArousal}}, \code{\link{CalcEnergyTimeEuthermic}}
 #' @author Katie Haase
 #'
-
 CalcCoolTime <- function(Ta, bat.params){
   with(bat.params,{
     ifelse(Ta > Ttormin,
