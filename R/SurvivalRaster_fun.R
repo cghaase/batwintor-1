59a328b12da5165163a2a08387d818b474d54ce2
Carter Reed Hranac <crh244@gmail.com>
2017-08-28 13:42:37 +1200

    SurvialRaster speed issue fixed

--- R/SurvivalRaster_fun.R
+++ R/SurvivalRaster_fun.R
@@ -1,7 +1,9 @@
 #' Converts model results dataframe into results raster.
 #'
 #' \code{SurvivalRaster} uses supplied environmental rasters to query model
-#' results and creating a raster filled with maximum hibernation estimates.
+#' results and creating a raster filled with maximum hibernation estimates for
+#' hibernation without infection, with infection and the difference between the
+#' two.
 #'
 #' @param mod.df results from \code{\link{DynamicEnegryPd}}
 #' @param hum.rast raster of humidity values for extent
@@ -13,31 +15,75 @@
 #' humidity rasters in teh original Hayman et al. 2016 represent the average
 #' of the average humidities for the months Jan-Mar.
 #'
-#' @note This function as is is pretty much shite and takes for.ev.er. use at
-#' your own risk, and really try to do it only once.
+#' @note
 
 SurvivalRaster <- function(mod.df, hum.rast, temp.rast){
   require(raster);require(dplyr)
+
+  #Raster modifications
   temp.c <- temp.rast - 273
   out <- raster(hum.rast); values(out) <- NA
-  out.s <- list(out,out); names(out.s) <- c("max.inf", "max.null")
+  out.s <- stack(out,out,out); names(out.s) <- c("max.inf", "max.null", "diff")
+
   mod.dif <- mod.df %>%
     dplyr::group_by(Ta, humidity) %>%
-    dplyr::summarise(max.null = max(time*surv.null),max.inf = max(time*surv.inf)) %>%
-    dplyr::mutate(diff = (max.inf - max.null)/(24*30)) %>%
+    dplyr::summarise(max.null = max(time*surv.null)/24, max.inf = max(time*surv.inf)/24) %>%
+    dplyr::mutate(diff = (max.null-max.inf)/24) %>%
     ungroup %>% data.table
-  out.l <- c()
-  for(j in 1:2){
-    ifelse(j == 1, sel <- "max.inf", sel <- "max.null")
-    for(i in 1:ncell(hum.rast)){
-      #values(out.s[j][i])
-      out.l[[i]] <- min(mod.dif[which(abs(mod.dif$humidity-hum.rast[i]) ==
-                                 min(abs(mod.dif$humidity-hum.rast[i]))
-                          & abs(mod.dif$Ta-temp.c[i]) ==
-                            min(abs(mod.dif$Ta-temp.c[i]))),
-                          sel, with = FALSE])
+
+  #Vectors for look up table structure
+  Ta_vals <- unique(mod.dif$Ta)
+  humidity_vals <- unique(mod.dif$humidity)
+
+  #Look Up Table
+  lut <- array(NA, dim=c(length(Ta_vals), length(humidity_vals), 3)) # 3 for max.inf, max.null, diff
+  dimnames(lut)[[1]] <- Ta_vals
+  dimnames(lut)[[2]] <- humidity_vals
+  dimnames(lut)[[3]] <- c("max.inf", "max.null", "diff")
+
+  #Fill look up table
+  for (i in seq_len(nrow(mod.dif))) {
+    d <- mod.dif[i,]
+    if (i %% 1000 == 0) {
+      cat("up to", i, "of", nrow(mod.dif), "\n")
     }
-    values(out.s[[j]]) <- out.l
+    lut[as.character(d$Ta), as.character(d$humidity),] <- c(d$max.inf,
+                                                            d$max.null, d$diff)
   }
+
+  # Find the closest item in the vector y to x.
+  # NOTE: Assumes that y is increasing, equi-spaced vector
+  find_closest <- function(x, y) {
+    dy <- (y[length(y)] - y[1]) / (length(y)-1)
+    wch <- round((x - y[1]) / dy + 1)
+    # check the range.
+    clamp <- function(x, xmin, xmax) {
+      min(max(x, xmin),xmax)
+    }
+    clamp(wch, 1, length(y))
+  }
+
+  #Extract data from rasters  to matrix for speed
+  # pull data out of the raster
+  hum <- as.matrix(hum.rast)
+  temp <- as.matrix(temp.c)
+
+  #Run lookup
+  out.l <- list()
+  for(i in 1:length(hum)){
+    # first find the closest humidity and Ta
+    if(i %% 1000 == 0){
+      cat("up to", i, "of", length(hum), "\n")
+    }
+    hum_i <- find_closest(hum[i], humidity_vals)
+    Ta_i  <- find_closest(temp[i], Ta_vals)
+    # lookup the values in our LUT
+    out.l[[i]] <- lut[Ta_i, hum_i,]
+  }
+
+  #Return values to matrix
+  out.m <- do.call(rbind, out.l)
+  out.s <- setValues(out.s, out.m)
+
   return(out.s)
 }
