15462b19162e560e0fca2324d35b618a85188ac1
Carter Reed Hranac <crh244@gmail.com>
2017-12-07 10:51:31 +1300

    Update for Parma files with mlO2

--- data-raw/batData.R
+++ data-raw/batData.R
@@ -1,8 +1,6 @@
 "bat.params"
-if("bat.params.rda" %!in% list.files("data/")){
- #coolated data from study and literature. Some missing values were filled where needed
-  bat.params <- read.csv("data-raw/batParamsFilled.csv", stringsAsFactors = F, header = T,
+#coolated data from study and literature. Some missing values were filled where needed
+bat.params <- read.csv("data-raw/batParamsFilled.csv", stringsAsFactors = F, header = T,
                   row.names = 1)
 
-  devtools::use_data(bat.params, overwrite = T)
-}
+devtools::use_data(bat.params, overwrite = T)
--- data-raw/myluData.R
+++ data-raw/myluData.R
@@ -1,7 +1,6 @@
 "mylu.params"
-if("mylu.params.rda" %!in% list.files("data/")){
-  bat.params <- read.csv("data-raw/batParamsFilled.csv", stringsAsFactors = F, header = T,
+bat.params <- read.csv("data-raw/batParamsFilled.csv", stringsAsFactors = F, header = T,
                          row.names = 1)
-  mylu.params <- bat.params["mylu",]
-  devtools::use_data(mylu.params, overwrite = T)
-}
+mylu.params <- bat.params["mylu",]
+devtools::use_data(mylu.params, overwrite = T)
+
