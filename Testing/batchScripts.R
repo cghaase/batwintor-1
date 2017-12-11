001965a5cb9d0d9c1090d3f964896a978138cb91
Carter Reed Hranac <crh244@gmail.com>
2017-12-11 17:08:34 +1300

    batch working

--- Testing/batchScripts.R
+++ Testing/batchScripts.R
@@ -2,7 +2,7 @@
 library(batwintor)
 
 
-WNS_run <- function(species, out.name){
+WNS_run <- function(species){
   ## Function to run a single DynamicEnergyPD
   ## Change Env as needed
 
@@ -12,32 +12,33 @@ WNS_run <- function(species, out.name){
   data("fung.params")
   fung.ch <- FungSelect(choose =  "Chaturvedi")
 
-  env <- BuildEnv(temp = c(-1,10),
-                  pct.rh = c(90,100),
+  env <- BuildEnv(temp = c(-5,20),
+                  pct.rh = c(20,100),
                   range.res.temp = .5,
                   range.res.rh = 1,
-                  twinter = 6,
+                  twinter = 10,
                   winter.res = 24)
   z <- DynamicEnergyPd(env = env,
                   bat.params = bat.p,
                   fung.params = fung.ch)
-  write.csv(z, file = out.name, row.names = F)
+  write.csv(z, file = file.path("D:", "Dropbox",
+                                "batwintor_aux", "Run_10_12",
+                                paste0(species, "DNEpd.csv")), row.names = F)
 
 }
 
 
-WNS_batch <- function(species.l, out.name){
+WNS_batch <- function(species.l){
   ## Function for running a batch of WNS scripts (from bat.param file)
   ## species.l <- should be row.names(bat.params)
   ## out.dir is a foulder where it should output too (will auto name the files)
-  out.name <- file.path(out.name,
-                        paste0(species.l, "DNEpd.csv"))
+  # out.name <- file.path(out.name,
+  #                       paste0(species.l, "DNEpd.csv"))
   library(snowfall)
   sfInit(parallel=TRUE, cpus = 3)
   sfLibrary('batwintor', character.only = T)
   sfExportAll()
-  sfLapply(species.l, WNS_run,
-           out.name = out.name)
+  sfLapply(species.l, WNS_run)
   sfStop(nostop = FALSE)
 
 }
@@ -45,7 +46,5 @@ WNS_batch <- function(species.l, out.name){
 species.l <- row.names(bat.params)
 out.name <- file.path("D:", "Dropbox",
                       "batwintor_aux", "Run_10_12")
-WNS_batch(species.l = species.l,
-          out.name = out.name)
-species <- species.l[[1]]
-WNS_run(spec, out.name)
+WNS_batch(species.l = species.l)
+
