80beb011083fe2723ee9cf741a2f9a06c135506c
Carter Reed Hranac <crh244@gmail.com>
2018-06-25 16:32:49 -0600

    Example scripts
    updating example scripts

--- ExampleScripts/torporEnergy_ex.R
+++ ExampleScripts/torporEnergy_ex.R
@@ -2,8 +2,11 @@
 data("mylu.params")
 
 e <- -10:34 #temperature vector
+a <- seq(from = .001, to = 3, length.out = 45) #area vector
 
-r <- torporEnergy(e, mylu.params)
-
+r <- torporEnergy(Ta = e, WNS = F, areaPd = 0, bat.params = mylu.params)
+r1 <- torporEnergy(Ta = rep(4,45), areaPd = a, WNS = T, bat.params = mylu.params)
 plot(e, r, xlab = "Temp (C)", ylab = "Energy Expended",
+     main = "torporEnergy")
+plot(a, r1, xlab = "Area Infected (cm2^)", ylab = "Energy Expended",
      main = "torporEnergy")
--- ExampleScripts/torporTime_ex.R
+++ ExampleScripts/torporTime_ex.R
@@ -1,15 +1,14 @@
 ##CalcTorporTIme Example
 data("mylu.params")
-
+fung <- fungalSelect("Chaturvedi")
 e <- -10:34 #temperature vector
+h <- 56:100 #humidity vector
 a <- seq(from = .001, to = 3, length.out = 45) #area vector
 
-torporTime(Ta = 4, areaPd = 0, inf = TRUE, bat.params = mylu.params)
-
-r  <- torporTime(Ta = e, areaPd = rep(0,45), inf = FALSE, bat.params = mylu.params)
-r1 <- torporTime(Ta = rep(4,45), areaPd = a, inf = TRUE, bat.params = mylu.params)
+r <- torporTime(Ta = e, pct.rh = h, areaPd = 0, WNS = F, bat.params = mylu.params, fung.params = fung)
+r1 <- torporTime(Ta = rep(4,45), pct.rh = 95,  areaPd = a, WNS = TRUE, bat.params = mylu.params, fung.params = fung)
 
 plot(e, r, xlab = "Temp (C)", ylab = "Time in Torpor",
-     main = "torporTIme")
+     main = "torporTime")
 plot(a, r1, xlab = "Area Infected (cm2^)", ylab = "Time in Torpor",
-     main = "torporTIme")
+     main = "torporTime")
