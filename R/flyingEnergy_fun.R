779811e03c0dda2b414f519210b9ea9d4de445a0
cghaase <cghaase@gmail.com>
2018-07-31 15:52:03 -0600

    Weird issues.

--- R/flyingEnergy_fun.R
+++ R/flyingEnergy_fun.R
@@ -20,10 +20,10 @@ flyingEnergy <- function(Ta, bat.params){
     Kfur = 4.154 + (1.3*(3.7^1))                      #Forced convective coefficient
     Tsky = Ta - 20.4 + (0.22*Ta)                      #temperature of atmospheric sky
     esky = 0.398 * (10^-5) * ((Ta + 273.15)^2.148)    #emissivity of sky
-
+    #Calculate different costs per body part
     Efly_wing = (44.27*(Tb-Ta)) + (5*10^-8)*0.98*esky*(((Tb+273.15)^4)-((Tsky+273.15)^4))*SA.wing
     Efly_body = (Kfur*(Teu-Ta)) + (5*10^-8)*0.98*esky*(((Teu+273.15)^4)-((Tsky+273.15)^4))*SA.body
     Efly_all  = 0.2*Pf + Efly_body + Efly_wing
-    Efl = (((Efly_all*0.0143*60)/5.05))/Mass        #Convert from Watts to ml O2 per g
+    Efl = (((Efly_all*0.0143*60)/5.05))/Mass       #Convert from Watts to ml O2 per g
     return(Efl)
   })}
